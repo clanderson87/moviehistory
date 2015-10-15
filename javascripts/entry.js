688e0428878256bc01506691bd4323a08d11dc61
Joel Harris <joelharris@Joels-Air.attlocal.net>
2015-10-14 23:30:10 -0500

    fixed gitignore and started js process

---
+++ javascripts/entry.js
@@ -0,0 +1,28 @@
+requirejs.config({
+	baseUrl: "./javascripts",
+	paths:{
+		"jquery": "../lib/bower_components/jquery/dist/jquery.min",
+		"firebase":"../lib/bower_components/firebase/firebase",
+		"hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+		"lodash": "../lib/bower_components/lodash/lodash",
+		"q": "../bower_components/q/q",
+		"bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap"
+	},
+	shim:{
+		"bootstrap":["jquery"],
+		'firebase':{
+			exports:'Firebase'
+		}
+	}
+});
+
+//this is the starting point telling it which javacsript files to reference
+
+require(["jquery","bootstrap"], function($){
+	$(document).on('click', '#login', function(){
+		console.log("logged in!")
+	})
+	$(document).on('click', '#register', function(){
+		console.log("logged in!")
+	})
+});
