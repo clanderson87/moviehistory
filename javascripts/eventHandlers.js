0edd0e417b9b4a031a912cd93024c0fbec547103
Chris <cl.anderson8@gmail.com>
2015-10-17 11:27:39 -0500

    adding index modal, and eventHandler.js

--- javascripts/eventHandlers.js
+++ javascripts/eventHandlers.js
@@ -1,27 +1,41 @@
 define(["jquery", "require-handlebars-plugin"] function $, hbs {
 
   //this is the even handler for search OMDB button
-    $("#searchOMDB").on
+    $(document).on('click, keypress', '#searchOMDB', function(){
+      var title = $('#title').val();
+        omdb.getomdb(title);
+    })
+
+    $(document).keypress(function(e) {
+      if(e.which == 13) {
+      var title = $('#title').val();
+        omdb.getomdb(title);
+    }
 
   //this is the event handler for the search My Movies
-    $("#searchMyMov").on
+    $(document).on('click', '#searchMyMov', function(){
+
+    })
 
   //this is the event handler for the display all button pill
-    $("#displayAll").on
+    $(document).on('click', '#displayAll', function(){
+
+    })
 
   //this is the event handler for the display watched pill
-    $("#displayWatched").on
+    $(document).on('click', '#displayWatched', function (){
+
+    })
 
   //this is the event handler for the display to watch pill
-    $("#displayToWatch").on
+    $(document).on('click', '#displayToWatch' function (){
+      ()
+    })
 
-  //this is the event handler for the hbs template "add" button
-    $("#addMovie")
-
-  //this is the event handler for the hbs template "watched" button
-    $("#addToWatched").on
-
-  //this is the event handler for the rating slider
-    $(#"rating").on
-
+  //this is the event handler for the rating slider, "watched" button, "add" button
+    $(document).on('click', '#towatch, #watched, #rating', function(){
+        update.updateuser(uid);
+        retrieve.retreiveuserdata(uid);
+        console.log("added to", uid);
+  });
 }
