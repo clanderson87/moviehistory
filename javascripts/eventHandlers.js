ef6b54406b9eadbc158bed0380df2ba383b07d33
Chris <cl.anderson8@gmail.com>
2015-10-17 10:32:26 -0500

    updated index tmplate, eventHandlers.js

---
+++ javascripts/eventHandlers.js
@@ -0,0 +1,27 @@
+define(["jquery", "require-handlebars-plugin"] function $, hbs {
+
+  //this is the even handler for search OMDB button
+    $("#searchOMDB").on
+
+  //this is the event handler for the search My Movies
+    $("#searchMyMov").on
+
+  //this is the event handler for the display all button pill
+    $("#displayAll").on
+
+  //this is the event handler for the display watched pill
+    $("#displayWatched").on
+
+  //this is the event handler for the display to watch pill
+    $("#displayToWatch").on
+
+  //this is the event handler for the hbs template "add" button
+    $("#addMovie")
+
+  //this is the event handler for the hbs template "watched" button
+    $("#addToWatched").on
+
+  //this is the event handler for the rating slider
+    $(#"rating").on
+
+}
